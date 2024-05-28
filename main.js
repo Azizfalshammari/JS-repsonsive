8aebd1b62f24ed12f15000acbcba0b3596933cb0
Azizfalshammari <azizfalshammari.com>
2024-05-28 17:01:37 +0300

    done

--- main.js
+++ main.js
@@ -10,28 +10,36 @@ let ipad = document.getElementById('ipadpro');
 let mac = document.getElementById('mcosx');
 let linker = document.getElementById('form1')
 
+let linktogo =linker.value
 
-let linktogo =linker.va
+console.dir(linktogo)
 iphone.addEventListener('click', ()=>{
-    window.open(linktogo);
+    
+linktogo =linker.value
+    window.open(linktogo, height=375,width= 812);
+    console.dir(linktogo)
 
 
 })
 
 note.addEventListener('click', ()=>{
-    window.open(linktogo);
-
+     linktogo =linker.value
+    window.open(linktogo, height=412,width= 869);
 
 })
 
 ipad.addEventListener('click', ()=>{
-    window.open(linktogo);
+    
+ linktogo =linker.value
+    window.open(linktogo, height=1024,width= 1366);
 
 
 })
 
 mac.addEventListener('click', ()=>{
-    window.open(linktogo);
+    
+linktogo =linker.value
+    window.open(linktogo, height=1920,width= 1080);
 
 
 })
